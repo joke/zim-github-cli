845a17d51bf00a9e4c6f9398986ec82a088af01a
Joke de Buhr <joke@xckk.de>
2022-09-16 13:25:09 +0200

    fix: asdf command fallback
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -1,6 +1,6 @@
 (( ${+commands[gh]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
 
-  local command=${commands[gh]:-"$(${commands[asdf]} which kn 2> /dev/null)"}
+  local command=${commands[gh]:-"$(${commands[asdf]} which gh 2> /dev/null)"}
   [[ -z $command ]] && return 1
 
   local compfile=$1/functions/_gh
