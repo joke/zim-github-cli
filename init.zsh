c9dd3c4d635d91fbf2be806fc0a1a6011ca1beb6
Joke de Buhr <joke@xckk.de>
2023-03-12 15:59:31 +0100

    fix: new version detection message
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -6,6 +6,6 @@
   local compfile=$1/functions/_gh
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completion -s zsh >| $compfile
-    zimfw check-dumpfile
+    print -u2 -PR "* Detected a new version 'gh'. Regenerated completions."
   fi
 } ${0:h}
